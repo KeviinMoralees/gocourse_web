0353442e706481a396c521778d97208aee5f4570
KeviinMoralees <keviinmoralees132@gmail.com>
2025-06-17 20:07:13 -0500

    commit antes de la arquitectura

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KeviinMoralees/gocourse_web
 
 go 1.24
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
 func main() {
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/courses", getCourses).Methods("GET")
+
+	srv := &http.Server{
+		Handler:      http.TimeoutHandler(router, 3*time.Second, "Timeout"),
+		Addr:         "127.0.0.1:8000",
+		WriteTimeout: 5 * time.Second,
+		ReadTimeout:  5 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(6 * time.Second)
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
+
+func getCourses(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
 }
